84f8b668b5e17d9050889f58790f4d8254e0d2a6
Boris <lozinin1998@gmail.com>
2025-10-21 03:04:17 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const env = require('dotenv').config().parsed;
+const morgan = require('morgan');
 
 
 const app = express();
 const routes = require('./routes.js');
+
+if (env.STAGE !== 'production') {
+  app.use(morgan('dev'));         
+} else {
+  app.use(morgan('combined'));  
+}
 
 app.use(express.json());
 app.use(routes);
